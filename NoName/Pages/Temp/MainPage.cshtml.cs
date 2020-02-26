764b814a0b65920c9da66c8319326fd87008d5cc
vamous <rlagudtn4510@naver.com>
2020-02-26 23:43:28 +0900

    Connect mainpage with Myposts, comments,scrap

---
+++ NoName/Pages/Temp/MainPage.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NoName.Pages.Temp
+{
+    public class MainPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
