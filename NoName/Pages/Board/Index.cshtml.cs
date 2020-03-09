dc0149704779eb2d72e1e360ed23d8746ca491fc
vamous <rlagudtn4510@naver.com>
2020-03-09 21:50:17 +0900

    진행중

--- NoName/Pages/Board/Index.cshtml.cs
+++ NoName/Pages/Board/Index.cshtml.cs
@@ -4,17 +4,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using NoName.Data;
+using NoName.Data.DbData;
 
 namespace NoName.Pages.Board
 {
     public class IndexModel : PageModel
     {
+        private readonly NoName.Data.DbData.DataContext _context;
 
-       
-        public void OnGet()
+        public  IndexModel(NoName.Data.DbData.DataContext context) {
+            _context = context;
+        }
+        [BindProperty]
+        public TablePost TablePost { get; set; }
+
+        public IList<TablePost> Board { get; set; }
+        public async void OnGetAsync()
         {
+            Board = await _context.Post.ToListAsync();
+        }
 
+        public async Task<IActionResult> OnPostAsync() {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //DataDbManager.GetInstance().DataDB. 
+
+            _context.Post.Add(TablePost);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
         }
     }
 }
